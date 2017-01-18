d8c7797a7e38fcd0728ce968145a1f6b83ec1897
Thomas Berger <thomas@epicentral.de>
2017-01-19 00:09:35 +0100

    Also adopted start class name to new project name

--- src/main/java/de/thberger/bitcore/BitCoreApplication.java
+++ src/main/java/de/thberger/bitcore/BitCoreApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JigtApplication {
+public class BitCoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JigtApplication.class, args);
+		SpringApplication.run(BitCoreApplication.class, args);
 	}
 }
--- src/test/java/de/thberger/bitcore/BitCoreApplicationTests.java
+++ src/test/java/de/thberger/bitcore/BitCoreApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class JigtApplicationTests {
+public class BitCoreApplicationTests {
 
 	@Test
 	public void contextLoads() {
